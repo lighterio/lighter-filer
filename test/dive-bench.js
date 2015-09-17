0a7b57a6e8964aa2c30c014972172dd858bd2865
Sam Eubank <sameubank+zerious@gmail.com>
2015-09-17 08:23:49 -0700

    Initial commit

---
+++ test/dive-bench.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+var dive = require(__dirname + '/lighter-dive')
+
+describe('Benchmarks', function () {
+
+  bench('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
---
+++ test/dive-test.js
@@ -0,0 +1,26 @@
+var dive = require('../lighter-dive')
+var is = global.is || require('exam/lib/is')
+
+describe('dive', function () {
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
